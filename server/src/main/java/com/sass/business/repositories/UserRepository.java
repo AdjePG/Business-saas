a848764dc1762418bbddaba4fd871c50a2e44d6a
Marc <marclopezmarco469@gmail.com>
2024-06-19 12:08:35 +0200

    Update UserRepository.java

--- server/src/main/java/com/sass/business/repositories/UserRepository.java
+++ server/src/main/java/com/sass/business/repositories/UserRepository.java
@@ -8,8 +8,5 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-
+    
 }
