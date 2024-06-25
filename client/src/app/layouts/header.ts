5734352e83bdebe2e606e641efb6e2d3aa1081b0
Adri PG <adriprog2002@gmail.com>
2024-06-25 12:09:52 +0200

    Modyfing client header and removing theme customizer

--- client/src/app/layouts/header.ts
+++ client/src/app/layouts/header.ts
@@ -40,44 +40,6 @@ export class HeaderComponent {
             time: '9h Ago',
         },
     ];
-    messages = [
-        {
-            id: 1,
-            image: this.sanitizer.bypassSecurityTrustHtml(
-                `<span class="grid place-content-center w-9 h-9 rounded-full bg-success-light dark:bg-success text-success dark:text-success-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg></span>`
-            ),
-            title: 'Congratulations!',
-            message: 'Your OS has been updated.',
-            time: '1hr',
-        },
-        {
-            id: 2,
-            image: this.sanitizer.bypassSecurityTrustHtml(
-                `<span class="grid place-content-center w-9 h-9 rounded-full bg-info-light dark:bg-info text-info dark:text-info-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg></span>`
-            ),
-            title: 'Did you know?',
-            message: 'You can switch between artboards.',
-            time: '2hr',
-        },
-        {
-            id: 3,
-            image: this.sanitizer.bypassSecurityTrustHtml(
-                `<span class="grid place-content-center w-9 h-9 rounded-full bg-danger-light dark:bg-danger text-danger dark:text-danger-light"> <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></span>`
-            ),
-            title: 'Something went wrong!',
-            message: 'Send Reposrt',
-            time: '2days',
-        },
-        {
-            id: 4,
-            image: this.sanitizer.bypassSecurityTrustHtml(
-                `<span class="grid place-content-center w-9 h-9 rounded-full bg-warning-light dark:bg-warning text-warning dark:text-warning-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">    <circle cx="12" cy="12" r="10"></circle>    <line x1="12" y1="8" x2="12" y2="12"></line>    <line x1="12" y1="16" x2="12.01" y2="16"></line></svg></span>`
-            ),
-            title: 'Warning',
-            message: 'Your password strength is low.',
-            time: '5days',
-        },
-    ];
 
     constructor(
         public translate: TranslateService,
@@ -129,10 +91,6 @@ export class HeaderComponent {
 
     removeNotification(value: number) {
         this.notifications = this.notifications.filter((d) => d.id !== value);
-    }
-
-    removeMessage(value: number) {
-        this.messages = this.messages.filter((d) => d.id !== value);
     }
 
     changeLanguage(item: any) {
--- server/src/main/java/com/sass/business/models/User.java
+++ server/src/main/java/com/sass/business/models/User.java
@@ -1,14 +1,11 @@
 package com.sass.business.models;
 
-import com.sass.business.others.Role;
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.UUID;
 
 @Entity
 @Table(name = "users")
